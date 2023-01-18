413f353d538e39ee282f0589b8c386c6a99091fe
Jose Pedro <jp14274@gmail.com>
2023-01-18 12:01:46 -0300

    feat: entendo mais sobre Models

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produto;
 
 class ProdutoController extends Controller
 {
@@ -15,6 +16,8 @@ class ProdutoController extends Controller
     {
         //
         return 'index';
+        $produtos = Produto::all();
+        return dd($produtos);
     }
 
     /**
