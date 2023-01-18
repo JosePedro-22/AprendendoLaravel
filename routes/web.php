9c8e5b545077b91903de9fd8d17969c84c18eaf5
Jose Pedro <jp14274@gmail.com>
2023-01-18 10:20:17 -0300

    feat: entendendo  mais sobre rotas, controller, Conexao de Banco de Dados

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProdutoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,54 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::resource('produtos', ProdutoController::class);
+
+/* Route::get('/', [ProdutoController::class, 'index'])->name('produto.index');
+
+Route::get('/produto/{id?}', [ProdutoController::class, 'show'])->name('produto.show'); */
+/* Route::get('/empresa', function () {
+    return view('site/empresa');
+}); */
+/* Route::view('/empresa','site/empresa');
+
+Route::any('/any', function () {
+    return "permite toda de acesso http(get, post, put, delete)...";
 });
+
+Route::match(["put","delete"],'/match', function () {
+    return "permite apenas acesso definido...";
+});
+
+Route::get('/produto/{id}/{cat?}', function ($id, $cat = '') {
+    return "O id do produto é: ".$id."<br>"."<h2>A catergoria do produto é: </h2>".$cat;
+}); */
+
+/* Route::redirect('/welcome', '/empresa');
+
+Route::get('/news',function(){
+    return  view('site/news');
+})->name('noticias');
+
+Route::get('/novidades', function(){
+    return redirect()->route('noticias');
+}); */
+
+/* admin/ se repeti em todas as rotas... podemos deixar prefixado */
+/* Route::group([
+    'prefix' => 'admin', chave prefix
+    'as' => 'admin.' chave informando o nome das rotas do grupo
+    ], function () {
+
+    Route::get('dashboard', function(){
+        return "dashboard";
+    })->name('dashboard');
+
+    Route::get('help', function(){
+        return "help";
+    })->name('help');
+
+    Route::get('sobre', function(){
+        return "sobre";
+    })->name('sobre');
+});
+ */
