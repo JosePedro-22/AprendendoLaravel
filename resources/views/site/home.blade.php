b50a4e8a2443f03a79ead893ada09a1856ff96fe
Jose Pedro <jp14274@gmail.com>
2023-01-20 10:53:15 -0300

    feat: estilizando com materialize

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,8 +1,81 @@
 @extends('site.layout')
-
 @section('title', 'Essa é a nossa HOME')
-
 @section('conteudo')
+    <div class="row container">
+        <div class="col s12 m3">
+            <div class="card">
+                <div class="card-image">
+                  <img src="images/sample-1.jpg">
+                  <span class="card-title">Card Title</span>
+                  <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">visibility</i></a>
+                </div>
+                <div class="card-content">
+                  <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                </div>
+              </div>
+        </div>
+        <div class="col s12 m3">
+            <div class="card">
+                <div class="card-image">
+                  <img src="images/sample-1.jpg">
+                  <span class="card-title">Card Title</span>
+                  <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                </div>
+                <div class="card-content">
+                  <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                </div>
+              </div>
+        </div>
+        <div class="col s12 m3">
+            <div class="card">
+                <div class="card-image">
+                  <img src="images/sample-1.jpg">
+                  <span class="card-title">Card Title</span>
+                  <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                </div>
+                <div class="card-content">
+                  <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                </div>
+              </div>
+        </div>
+        <div class="col s12 m3">
+            <div class="card">
+                <div class="card-image">
+                  <img src="images/sample-1.jpg">
+                  <span class="card-title">Card Title</span>
+                  <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                </div>
+                <div class="card-content">
+                  <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                </div>
+              </div>
+        </div>
+    </div>
+@endsection
+    {{-- <h1>Conteudo Home</h1> --}}
+
+   {{--  @switch($type)
+        @case(1)
+                false
+            @break
+        @case(25)
+                true
+            @break
+        @default
+
+    @endswitch --}}
+    {{-- @if ($type == 25)
+        ok <br/>
+    @endif
+
+    @auth
+        está autenticado no sistema <br/>
+    @endauth
+
+    @guest
+        nao está autenticado no sistema
+    @endguest --}}
+
     {{-- <h1>Conteudo Home</h1> --}}
 
    {{--  @switch($type)
@@ -28,12 +101,3 @@
     @endguest --}}
 
 
-{{--     uso do include  --}}
-@include('includes.mensagem', ['title' => 'Usuário Home'])
-
-@component('components.sidebar')
-    @slot('paragrafo')
-        Isto é um exemplo de paragrafo...
-    @endslot
-@endcomponent
-@endsection
--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -4,9 +4,29 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>@yield('title')</title>
+         <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <title>@yield('title')</title>
 	</head>
 	<body>
+
+        <nav class="green">
+            <div class="nav-wrapper container">
+              <a href="#" class="brand-logo center">Laravel</a>
+              <ul id="nav-mobile" class="left hide-on-med-and-down">
+                <li><a href="sass.html">Home</a></li>
+                <li><a href="badges.html">Carrinho</a></li>
+                {{-- <li><a href="collapsible.html">JavaScript</a></li> --}}
+              </ul>
+            </div>
+        </nav>
+
 		@yield('conteudo')
+
+        <script>
+            /* <!-- Compiled and minified JavaScript --> */
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        </script>
 	</body>
 </html>
