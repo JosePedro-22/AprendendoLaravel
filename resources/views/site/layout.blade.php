b36c877448f26524e22652dd8891f12dd08b9140
Jose Pedro <jp14274@gmail.com>
2023-01-27 10:57:16 -0300

    criando a funçao que tras do banco os nomes das categorias... e listando em tela.

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -1,32 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-         <!-- Compiled and minified CSS -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <title>@yield('title')</title>
-	</head>
-	<body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+        <!-- Dropdown Structure -->
+    <ul id='dropdown1' class='dropdown-content'>
+        @foreach ($categoriasMenu as $categoriaMenu)
+        <li><a href="#!">{{$categoriaMenu->nome}}</a></li>
+        @endforeach
+    </ul>
 
-        <nav class="green">
-            <div class="nav-wrapper container">
-              <a href="#" class="brand-logo center">Laravel</a>
-              <ul id="nav-mobile" class="left">
-                <li><a href="sass.html">Home</a></li>
-                <li><a href="badges.html">Carrinho</a></li>
-                {{-- <li><a href="collapsible.html">JavaScript</a></li> --}}
-              </ul>
-            </div>
-        </nav>
+    <nav class="green">
+        <div class="nav-wrapper container">
+            <a href="#" class="brand-logo center">Laravel</a>
+            <ul id="nav-mobile" class="left ">
+            <li><a href="sass.html">Home</a></li>
+            <li><a class='dropdown-trigger' href='#' data-target='dropdown1'>Categoria<i class="material-icons right">expand_more</i></a></li>
+            <li><a href="badges.html">Carrinho</a></li>
+            {{-- <li><a href="collapsible.html">JavaScript</a></li> --}}
+            </ul>
+        </div>
+    </nav>
 
-		@yield('conteudo')
+	@yield('conteudo')
 
-        <script>
-            /* <!-- Compiled and minified JavaScript --> */
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-        </script>
-	</body>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var elems = document.querySelectorAll('.dropdown-trigger');
+            var instances = M.Dropdown.init(elems, options);
+        });
+        $('.dropdown-trigger').dropdown();
+    </script>
+    <script>
+        /* <!-- Compiled and minified JavaScript --> */
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    </script>
+</body>
 </html>
