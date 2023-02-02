953d672f2468def6f9a9da5b04eb620294e4e695
Jose Pedro <jp14274@gmail.com>
2023-02-02 10:13:08 -0300

    feat: autorizacao por meio de policy

---
+++ app/Policies/ProdutoPolicy.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ProdutoPolicy{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function verProduto(User $user, Produto $produto){
+        return $user->id == $produto->user_id;
+    }
+}
