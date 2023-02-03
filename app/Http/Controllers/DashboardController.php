3dd354b9b6457944cb5223c397684c056554d734
Jose Pedro <jp14274@gmail.com>
2023-02-03 08:27:02 -0300

    feat: implementando os graficos com dados do banco de dados

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Categoria;
+use App\Models\Produto;
+
+use DB;
 
 class DashboardController extends Controller
 {
@@ -10,7 +15,38 @@ class DashboardController extends Controller
         $this->middleware('auth')->except(['index']);
     }
     public function index(){
+        $usuarios = User::all()->count();
 
-        return view('admin.dashboard');
+        //grafico 1 - usuarios
+        $usersData = User::select([
+            DB::raw('YEAR(created_at) as ano'),
+            DB::raw('COUNT(*) as total')
+        ])
+        ->groupBy('ano')
+        ->orderBy('ano', 'asc')
+        ->get();
+
+        //preparar arrays
+        foreach($usersData as $user){
+            $ano[] = $user->ano;
+            $total[] = $user->total;
+        }
+
+        $userLabel = "'Comparativo de cadastros de de usuarios'";
+        $userAno = implode(',', $ano);
+        $userTotal = implode(',', $total);
+
+
+        $catData = Categoria::all();
+
+        foreach($catData as $cat){
+            $catNome[] = "'".$cat->nome."'";
+            $catTotal[] = Produto::where('id_categoria', $cat->id)->count();
+        }
+
+        $catLabel = implode(',', $catNome);
+        $catTotal = implode(',', $catTotal);
+
+        return view('admin.dashboard', compact('usuarios', 'userLabel', 'userAno', 'userTotal', 'catLabel', 'catTotal'));
     }
 }
